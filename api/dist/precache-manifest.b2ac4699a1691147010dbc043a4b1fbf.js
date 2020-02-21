9feecdbb8c2001c1505b65dc61003403adde4c74
Agustin pablo Fiorenza <agustin.fiorenza@mercadolibre.com>
2020-02-21 14:45:41 -0300

    Build version

---
+++ api/dist/precache-manifest.b2ac4699a1691147010dbc043a4b1fbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8103408b091e124d60e1c5b1789acad8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f970452507e0b4fb836b",
+    "url": "/static/css/main.9b89cc57.chunk.css"
+  },
+  {
+    "revision": "00e2a5f9084df36d708a",
+    "url": "/static/js/2.8583ec3a.chunk.js"
+  },
+  {
+    "revision": "be8bd1997bc94dd87491f1847893fbfa",
+    "url": "/static/js/2.8583ec3a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f970452507e0b4fb836b",
+    "url": "/static/js/main.a04343f3.chunk.js"
+  },
+  {
+    "revision": "6b3defdcd169e5f902d5",
+    "url": "/static/js/runtime-main.1dc66141.js"
+  }
+]);
