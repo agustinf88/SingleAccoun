bb14a507b6d4426dee56842b16488cb6b4e9a794
Agustin pablo Fiorenza <agustin.fiorenza@mercadolibre.com>
2020-02-21 14:35:47 -0300

    use actions

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -5,9 +5,7 @@ const fetchAccountAction = action$ =>
   action$.pipe(
     filter(action => action.type === "FETCH_ACCOUNT"),
     mergeMap(async action => {
-      const response = await fetch("http://localhost:8080/account");
-      const data = await response.json();
-      console.log("FETCH_ACCOUNT_SUCCESS epic", data);
+      const data = await fetchAccount();
       return { type: "FETCH_ACCOUNT_SUCCESS", payload: data };
     })
   );
@@ -16,22 +14,11 @@ const addMovementAction = action$ =>
   action$.pipe(
     filter(action => action.type === "ADD_MOVEMENT"),
     mergeMap(async action => {
-      const { value } = action.payload;
-      console.log("ADD_MOVEMENT", action);
-      const response = await fetch("http://localhost:8080/account/movement", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ value: Number(value) })
-      });
-      if (response.status === 200) {
-        const data = await response.json();
-        console.log("ADD_MOVEMENT_SUCCESS", data);
+      try {
+        const { value } = action.payload;
+        const data = await postMovement(value);
         return { type: "ADD_MOVEMENT_SUCCESS", payload: data };
-      }
-
-      if (response.status === 403) {
+      } catch (error) {
         return { type: "FORBIDDEN_MOVEMENT", payload: "Can't add movement" };
       }
 
@@ -44,9 +31,7 @@ const addMovementSuccessAction = action$ =>
     filter(action => action.type === "ADD_MOVEMENT_SUCCESS"),
     mergeMap(async action => {
       try {
-        const response = await fetch("http://localhost:8080/account/balance");
-        const data = await response.json();
-        console.log("fetchBalance epic", data);
+        const data = await fetchBalance();
         return { type: "GET_BALANCE_SUCCESS", payload: data.balance };
       } catch (error) {
         console.error(error);
--- client/src/services/index.js
+++ client/src/services/index.js
@@ -1,7 +1,6 @@
 const fetchAccount = () =>
   fetch("http://localhost:8080/account")
     .then(res => res.json())
-    .then(data => data)
     .catch(err => console.error(err));
 
 const postMovement = async value =>
@@ -11,22 +10,20 @@ const postMovement = async value =>
       "Content-Type": "application/json"
     },
     body: JSON.stringify({ value: Number(value) })
-  })
-    .then(r => {
-      if (r.status === 200) {
-        return r.json();
-      }
+  }).then(r => {
+    if (r.status === 200) {
+      return r.json();
+    }
 
-      if (r.status === 403) {
-        return Promise.reject("Can't add movement");
-      }
-    })
-    .then(data => data)
-    .catch(err => err);
+    if (r.status === 403) {
+      return Promise.reject("Can't add movement");
+    }
+  });
 
 const fetchBalance = async () =>
   fetch("http://localhost:8080/account/balance")
     .then(r => r.json())
-    .then(data => data)
     .catch(err => console.error(err));
+
+    
 export { fetchAccount, postMovement, fetchBalance };
